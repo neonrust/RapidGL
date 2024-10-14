8d2b4d982fdd81fae7b6a05606cdafc05afd970a
André Jonsson <andre.jonsson@dataductus.se>
2024-10-14 12:19:08 +0200

    Changed ShaderStorageBuffer to be templated
    So the storage type is specified (and locked-in) at definition-time.
    
    Before, only the set() was templated, which enabled uploading
    completely different types of data at any time, which is a recipe for chaos...

--- src/core/ssbo.h
+++ src/core/ssbo.h
@@ -4,14 +4,26 @@
 #include <vector>
 
 
+template<typename T>
 class ShaderStorageBuffer
 {
 public:
-	ShaderStorageBuffer(GLuint bind_index=0);
-	~ShaderStorageBuffer();
+	using value_type = T;
 
-	template<typename T>
-	void set(const std::vector<T> &data, GLenum usage=GL_DYNAMIC_DRAW);
+public:
+	ShaderStorageBuffer(GLuint bind_index=0, GLenum default_usage=GL_DYNAMIC_DRAW) :
+		_bind_index(bind_index),
+		_default_usage(default_usage)
+	{
+	}
+	~ShaderStorageBuffer()
+	{
+		if(created())
+			glDeleteBuffers(1, &_id);
+		_id = GLuint(-1);
+	}
+
+	void set(const std::vector<T> &data, GLenum usage=0);
 
 private:
 	inline bool created() const { return _id != GLuint(-1); }
@@ -19,17 +31,18 @@ private:
 private:
 	GLuint _id { GLuint(-1) };
 	GLuint _bind_index { 0 };
+	GLenum _default_usage;
 };
 
 
 template<typename T>
-void ShaderStorageBuffer::set(const std::vector<T> &data, GLenum usage)
+void ShaderStorageBuffer<T>::set(const std::vector<T> &data, GLenum usage)
 {
 	const auto create = not created();
 	if(create)
 		glCreateBuffers  (1, &_id);
 
-	glNamedBufferData(_id, GLsizeiptr(sizeof(T) * data.size()), data.data(), usage);
+	glNamedBufferData(_id, GLsizeiptr(sizeof(T) * data.size()), data.data(), usage? usage: _default_usage);
 
 	if(create)
 		glBindBufferBase(GL_SHADER_STORAGE_BUFFER, _bind_index, _id);
