a74c0989d418835abb6e0a1cab3d23b2781875a8
André Jonsson <andre.jonsson@dataductus.se>
2024-10-20 15:17:38 +0200

    Formatting, mostly

--- src/core/mesh_part.h
+++ src/core/mesh_part.h
@@ -5,34 +5,36 @@
 
 namespace RGL
 {
-    constexpr static uint32_t INVALID_MATERIAL = 0xffffffff;
 
-    struct Vertex
-    {
-        Vertex() = default;
+constexpr static uint32_t INVALID_MATERIAL = std::numeric_limits<uint32_t>::max();
 
-        glm::vec3 m_position;
-        glm::vec3 m_normal;
-        glm::vec2 m_texcoord;
-        glm::vec3 m_tangent;
-    };
+struct Vertex
+{
+	Vertex() = default;
 
-    class MeshPart final
-    {
-    public:
-        MeshPart()
-            : m_base_vertex   (0),
-              m_base_index    (0),
-              m_material_index(INVALID_MATERIAL),
-              m_indices_count (0) { }
+	glm::vec3 m_position;
+	glm::vec3 m_normal;
+	glm::vec2 m_texcoord;
+	glm::vec3 m_tangent;
+};
 
-    private:
-        uint32_t m_base_vertex;
-        uint32_t m_base_index;
-        uint32_t m_material_index;
-        uint32_t m_indices_count;
+class MeshPart final
+{
+public:
+	MeshPart()
+		: m_base_vertex   (0),
+		m_base_index    (0),
+		m_material_index(INVALID_MATERIAL),
+		m_indices_count (0) { }
 
-        friend class StaticModel;
-        friend class AnimatedModel;
-    };
+private:
+	uint32_t      m_base_vertex;
+	uint32_t      m_base_index;
+	uint_fast16_t m_material_index;
+	size_t        m_indices_count;
+
+	friend class StaticModel;
+	friend class AnimatedModel;
+};
+
 }
