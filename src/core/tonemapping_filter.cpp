287a4e4a9e84e425bd65c13fe3230b02cdd24b98
André Jonsson <andre.jonsson@dataductus.se>
2024-12-02 13:52:13 +0100

    Made the tone mapping filter a bit more encapsulated

--- src/core/tonemapping_filter.cpp
+++ src/core/tonemapping_filter.cpp
@@ -4,32 +4,32 @@
 
 TonemappingFilter::TonemappingFilter(uint32_t width, uint32_t height)
 {
-	m_shader = std::make_shared<RGL::Shader>("src/demos/10_postprocessing_filters/FSQ.vert", "src/demos/27_clustered_shading/tmo.frag");
-	m_shader->link();
+	_shader = std::make_shared<RGL::Shader>("src/demos/27_clustered_shading/FSQ.vert", "src/demos/27_clustered_shading/tmo.frag");
+	_shader->link();
 
-	rt = std::make_shared<Texture2DRenderTarget>();
-	rt->create(width, height, GL_RGBA32F);
-	glTextureParameteri(rt->m_texture_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-	glTextureParameteri(rt->m_texture_id, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTextureParameteri(rt->m_texture_id, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	_rt = std::make_shared<Texture2DRenderTarget>();
+	_rt->create(width, height, GL_RGBA32F);
+	glTextureParameteri(_rt->texture_id(), GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTextureParameteri(_rt->texture_id(), GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTextureParameteri(_rt->texture_id(), GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-	glCreateVertexArrays(1, &m_dummy_vao_id);
+	glCreateVertexArrays(1, &_dummy_vao_id);
 }
 
 TonemappingFilter::~TonemappingFilter()
 {
-	if (m_dummy_vao_id)
-		glDeleteVertexArrays(1, &m_dummy_vao_id);
+	if (_dummy_vao_id)
+		glDeleteVertexArrays(1, &_dummy_vao_id);
 }
 
 void TonemappingFilter::bindTexture(GLuint unit)
 {
-	rt->bindTexture(unit);
+	_rt->bindTexture(unit);
 }
 
 void TonemappingFilter::bindFilterFBO(GLbitfield clear_mask)
 {
-	rt->bindRenderTarget(clear_mask);
+	_rt->bindRenderTarget(clear_mask);
 }
 
 void TonemappingFilter::render(float exposure, float gamma)
@@ -37,11 +37,11 @@ void TonemappingFilter::render(float exposure, float gamma)
 	glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	m_shader->bind();
-	m_shader->setUniform("u_exposure", exposure);
-	m_shader->setUniform("u_gamma", gamma);
+	_shader->bind();
+	_shader->setUniform("u_exposure", exposure);
+	_shader->setUniform("u_gamma", gamma);
 	bindTexture();
 
-	glBindVertexArray(m_dummy_vao_id);
+	glBindVertexArray(_dummy_vao_id);
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 }
--- src/core/tonemapping_filter.h
+++ src/core/tonemapping_filter.h
@@ -7,13 +7,7 @@ struct Texture2DRenderTarget;
 
 struct TonemappingFilter
 {
-	std::shared_ptr<RGL::Shader> m_shader;
-	std::shared_ptr<Texture2DRenderTarget> rt;
-
-	GLuint m_dummy_vao_id;
-
 	TonemappingFilter(uint32_t width, uint32_t height);
-
 	~TonemappingFilter();
 
 	void bindTexture(GLuint unit = 0);
@@ -21,4 +15,13 @@ struct TonemappingFilter
 	void bindFilterFBO(GLbitfield clear_mask = GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	void render(float exposure, float gamma);
+
+	const Texture2DRenderTarget &renderTarget() const { return *_rt; }
+	Texture2DRenderTarget &renderTarget() { return *_rt; }
+
+private:
+	std::shared_ptr<RGL::Shader> _shader;
+	std::shared_ptr<Texture2DRenderTarget> _rt;
+
+	GLuint _dummy_vao_id;
 };
