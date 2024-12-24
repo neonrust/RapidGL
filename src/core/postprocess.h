b473767749b4d86f8143206a17568d473397cadd
André Jonsson <andre.jonsson@dataductus.se>
2024-12-24 02:01:43 +0100

    Added 'enabled' flag

--- src/core/postprocess.h
+++ src/core/postprocess.h
@@ -13,7 +13,13 @@ class PostProcess
 public:
 	virtual operator bool () const = 0;
 
+	inline bool enabled() const { return _enabled; }
+	inline void setEnabled(bool enabled=true) { _enabled = enabled; }
+
 	virtual void render(const RenderTarget::Texture2d &in, RenderTarget::Texture2d &out) = 0;
+
+private:
+	bool _enabled { true };
 };
 
 } // RGL::PP
