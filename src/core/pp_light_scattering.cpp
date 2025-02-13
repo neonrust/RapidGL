885865817b8fe1d643f6d0d3f40fe9f09a269357
André Jonsson <andre.jonsson@dataductus.se>
2025-02-14 00:19:50 +0100

    'in' isn't actually used, and groups should be by 'out' size

--- src/core/pp_light_scattering.cpp
+++ src/core/pp_light_scattering.cpp
@@ -18,15 +18,14 @@ LightScattering::operator bool() const
 	return bool(_shader);
 }
 
-void LightScattering::render(const RenderTarget::Texture2d &in, RenderTarget::Texture2d &out)
+void LightScattering::render(const RenderTarget::Texture2d &, RenderTarget::Texture2d &out)
 {
-	in.bindImageRead(0);
 	out.bindImage(1, RGL::RenderTarget::Write);
 
 	_shader.bind();
 
-	glDispatchCompute(GLuint(glm::ceil(float(in.width()) / 8.f)),
-					  GLuint(glm::ceil(float(in.height()) / 8.f)),
+	glDispatchCompute(GLuint(glm::ceil(float(out.width()) / 8.f)),
+					  GLuint(glm::ceil(float(out.height()) / 8.f)),
 					  1);
 	glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
 }
