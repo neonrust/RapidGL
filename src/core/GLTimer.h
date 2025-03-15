e1fef6938243ae7c4a83dca6f1176c0b418007fd
André Jonsson <andre.jonsson@dataductus.se>
2025-03-15 15:44:30 +0100

    Added 'start_new_timer' argument to elapsed()
    When specified, it will call start() as well, so the calling code only
    need the one line. Useful if several time stamps are retrieved in a
    series.

--- src/core/GLTimer.h
+++ src/core/GLTimer.h
@@ -13,13 +13,18 @@ public:
 	void start();
 
 	template<typename D=std::chrono::nanoseconds>
-	D elapsed() const
+	D elapsed(bool start_new_timer=false) const
 	{
 		glEndQuery(GL_TIME_ELAPSED);
 		GLuint elapsed { 0 };
 		glGetQueryObjectuiv(_timer, GL_QUERY_RESULT, &elapsed);
 
-		return  std::chrono::duration_cast<D>(std::chrono::nanoseconds(elapsed));
+		const auto t = std::chrono::duration_cast<D>(std::chrono::nanoseconds(elapsed));
+
+		if(start_new_timer)
+			const_cast<GLTimer *>(this)->start();
+
+		return t;
 	}
 
 private:
