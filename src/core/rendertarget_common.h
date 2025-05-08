1b9545c142b6200a4abfde569ebf74e7f103aaa0
André Jonsson <andre.jonsson@dataductus.se>
2025-05-08 16:55:45 +0200

    I see I forgot to commit rendertarge_common.* files

---
+++ src/core/rendertarget_common.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "glad/glad.h"
+
+namespace RGL::RenderTarget
+{
+
+namespace Color
+{
+	using Config = uint32_t;
+
+	static constexpr Config None     = 0;
+	static constexpr Config Byte     = 0x0100'0000;
+	static constexpr Config Float    = 0x0400'0000;
+	static constexpr Config Texture  = 0x2000'0000;
+	static constexpr Config Default  = Float | Texture;
+
+	static constexpr Config custom_mask = 0x00ff'ffff; // anything in the lower bits is treated as a custom format
+	inline bool is_custom(Config f)
+	{
+		return (f & custom_mask) > 0;
+	}
+};
+
+namespace Depth
+{
+	using Config = uint32_t;
+
+	static constexpr Config None     = 0;
+	static constexpr Config Float    = 0x0400'0000;
+	static constexpr Config Texture  = 0x2000'0000;
+	static constexpr Config Default  = Float;
+}
+
+using BufferMask = uint32_t;
+static constexpr BufferMask NoBuffer = 0;
+static constexpr BufferMask ColorBuffer = GL_COLOR_BUFFER_BIT;
+static constexpr BufferMask DepthBuffer = GL_DEPTH_BUFFER_BIT;
+
+enum class Access : GLenum
+{
+	Read      = GL_READ_ONLY,
+	Write     = GL_WRITE_ONLY,
+	ReadWrite = GL_READ_WRITE
+};
+
+
+bool check_fbo(GLuint fbo_id);
+#if !defined(NDEBUG)
+void dump_config(const char *fbo_name, GLuint fbo);
+#endif
+
+} // RGL::RenderTarget
