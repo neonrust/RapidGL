2ccb327ccd252bc4dd755fec8c971b4cc594d547
André Jonsson <andre.jonsson@dataductus.se>
2025-06-24 00:19:39 +0200

    Renamed some methods; 2-word -> 3-word names
    And some comments

--- src/core/spatial_allocator.h
+++ src/core/spatial_allocator.h
@@ -40,10 +40,10 @@ public:
 
 	enum class NodeChild : uint32_t
 	{
-		TopLeft      = 1,
-		TopRight     = 2,
-		BottomLeft   = 3,
-		BottomRight  = 4
+		TopLeft      = 1,  // NW
+		TopRight     = 2,  // NE
+		BottomLeft   = 3,  // SW
+		BottomRight  = 4   // SE
 	};
 	struct Node
 	{
@@ -67,7 +67,7 @@ public:
 	[[nodiscard]] inline AxisT size() const { return _size; }
 
 	[[nodiscard]] NodeIndex allocate(AxisT size);
-	[[nodiscard]] NodeIndex allocate(AxisT size, AxisT min_size);  // demote to smaller size if necessary
+	[[nodiscard]] NodeIndex allocate(AxisT size, AxisT min_size);  // allow demotion to smaller size, if necessary
 	void free(NodeIndex index);
 
 	[[nodiscard]] Rect rect(NodeIndex index);
@@ -75,38 +75,32 @@ public:
 	[[nodiscard]] inline const AllocatedSlots &allocated() const { return _allocated; }
 
 	[[nodiscard]] uint32_t level(NodeIndex index) const;
-	[[nodiscard]] AxisT size(NodeIndex index) const;
+	[[nodiscard]] inline AxisT size(NodeIndex index) const { return _size >> level(index); }
 
 	[[nodiscard]] inline const Node &node(NodeIndex index) const { return _nodes[index]; }
 	[[nodiscard]] inline const Node &operator [] (NodeIndex index) const { return node(index); }
 
-	[[nodiscard]] static inline NodeIndex parent_index(NodeIndex index) { return index > 0? (index - 1) >> 2: BadIndex; }
-	[[nodiscard]] static inline NodeIndex child_index(NodeIndex index, NodeChild child=NodeChild::TopRLeft) {
-		return NodeIndex((uint32_t(index) << 2) + uint32_t(child));
+	[[nodiscard]] static inline NodeIndex parent_index(NodeIndex child) { return child > 0? (child - 1) >> 2: BadIndex; }
+	[[nodiscard]] static inline NodeIndex child_index(NodeIndex parent, NodeChild child=NodeChild::TopRLeft) {
+		return NodeIndex((uint32_t(parent) << 2) + uint32_t(child));
 	}
 	[[nodiscard]] static inline NodeChild node_child(NodeIndex index) { return NodeChild(uint32_t(index) & 3); }
 
 	[[nodiscard]] inline NodeIndex end() const { return BadIndex; }
 
 private:
-	[[nodiscard]] inline uint32_t size_level(AxisT size) const { assert(__builtin_popcount(size) == 1 and size < _size); return uint32_t(__builtin_ffs(int(_size / size)) - 1); }
-	[[nodiscard]] inline AxisT level_size(uint32_t level) const { return _size >> level; }
-	[[nodiscard]] static inline uint32_t levels_nodes(uint32_t levels) { return 1 << 2*levels; };
-	[[nodiscard]] static inline uint32_t level_start_index(uint32_t level) { return (levels_nodes(level) - 1)/3; };
-	[[nodiscard]] static uint32_t index_level(NodeIndex index) {
+	[[nodiscard]] inline uint32_t level_from_size(AxisT size) const { assert(__builtin_popcount(size) == 1 and size < _size); return uint32_t(__builtin_ffs(int(_size / size)) - 1); }
+	[[nodiscard]] inline AxisT size_of_level(uint32_t level) const { return _size >> level; }
+	[[nodiscard]] static inline uint32_t num_nodes_in_levels(uint32_t levels) { return 1 << 2*levels; };
+	[[nodiscard]] static inline uint32_t level_start_index(uint32_t level) { return (num_nodes_in_levels(level) - 1)/3; };
+	[[nodiscard]] static uint32_t level_from_index(NodeIndex index) {
 		const auto leading_zeroes = static_cast<uint32_t>(__builtin_clz(index * 3 + 1));
 		return (sizeof(index)*8 - leading_zeroes - 1)/2;
 	}
-	[[nodiscard]] static inline NodeIndex descendant_index(NodeIndex index, uint32_t skip_levels) {
-		assert(skip_levels > 0);
-		const auto current_level = index_level(index);
-		const auto level_offset = index - level_start_index(current_level);
-		const auto target_level_start = level_start_index(current_level + skip_levels);
-		const auto descendants_per_node = 1u << (2 * skip_levels);
-		const auto result = target_level_start + level_offset * descendants_per_node;
-		return result;
-	}
+	[[nodiscard]] static NodeIndex index_of_descendant(NodeIndex index, uint32_t skip_levels);
 	[[nodiscard]] inline Node &_node(NodeIndex index) { return _nodes[index]; }
+
+	// search the tree for a vacant "target_level"-sized slot
 	[[nodiscard]] NodeIndex find_available(uint32_t target_level, uint32_t current_level=0, NodeIndex index=0);
 
 private:
@@ -129,7 +123,7 @@ inline SpatialAllocator<AxisT>::SpatialAllocator(AxisT size, AxisT min_block_siz
 	assert(__builtin_popcount(_max_size) == 1);
 	assert(_min_size < _size);
 
-	auto num_levels = size_level(_min_size);
+	auto num_levels = level_from_size(_min_size);
 	auto num_nodes = level_start_index(num_levels);
 	assert(num_nodes < 65536); // more nodes seems a bit excessive don't you think?
 	_nodes.resize(num_nodes);
@@ -150,6 +144,7 @@ inline SpatialAllocator<AxisT>::NodeIndex SpatialAllocator<AxisT>::allocate(Axis
 {
 	assert(__builtin_popcount(size) == 1);
 	assert(__builtin_popcount(min_size) == 1);
+	assert(min_size <= size);
 
 	min_size = std::min(min_size, size);
 
@@ -163,7 +158,7 @@ inline SpatialAllocator<AxisT>::NodeIndex SpatialAllocator<AxisT>::allocate(Axis
 
 	auto allocated_size = size;
 	auto found = BadIndex;
-	for(auto lvl = size_level(size); lvl <= size_level(min_size); ++lvl)
+	for(auto lvl = level_from_size(size); lvl <= level_from_size(min_size); ++lvl)
 	{
 		found = find_available(lvl);
 		if(found != BadIndex)
@@ -205,7 +200,7 @@ SpatialAllocator<AxisT>::NodeIndex SpatialAllocator<AxisT>::find_available(uint3
 	const auto &n = _node(index);
 
 	// skip branches that are fully allocated
-	if (n.allocated or n.children_allocated == levels_nodes(target_level - current_level))
+	if (n.allocated or n.children_allocated == num_nodes_in_levels(target_level - current_level))
 	{
 		// std::print("{:{}}  [sa] {}  branch not available ({} + {})\n", "", s_indent, index, n.allocated, n.children_allocated);
 		return BadIndex;
@@ -222,8 +217,8 @@ SpatialAllocator<AxisT>::NodeIndex SpatialAllocator<AxisT>::find_available(uint3
 	// if the whole subtree is available, we can skip to the first child at target_level
 	if(not n.allocated and n.children_allocated == 0)
 	{
-		const auto skip_to_index = descendant_index(index, target_level - current_level);
-		assert(index_level(skip_to_index) == target_level);
+		const auto skip_to_index = index_of_descendant(index, target_level - current_level);
+		assert(level_from_index(skip_to_index) == target_level);
 		// std::print("{:{}}  [sa] direct hop {} -> {}  (level {} -> {})\n", "", s_indent, index, skip_to_index, current_level, target_level);
 		return skip_to_index;//find_available(target_level, target_level, skip_to_index);
 	}
@@ -250,7 +245,7 @@ void SpatialAllocator<AxisT>::free(NodeIndex index)
 	assert(n.children_allocated == 0);
 	n.allocated = false;
 
-	const auto allocated_size = level_size(index_level(index));
+	const auto allocated_size = size_of_level(level_from_index(index));
 	--_allocated[allocated_size];
 
 	// decrement child allocation counter in ancestors
@@ -266,7 +261,7 @@ void SpatialAllocator<AxisT>::free(NodeIndex index)
 template<typename AxisT>
 inline SpatialAllocator<AxisT>::Rect SpatialAllocator<AxisT>::rect(NodeIndex index)
 {
-	const auto level = index_level(index);
+	const auto level = level_from_index(index);
 	if(level == 0)
 		return { 0, 0, _size, _size };
 
@@ -286,15 +281,22 @@ inline SpatialAllocator<AxisT>::Rect SpatialAllocator<AxisT>::rect(NodeIndex ind
 }
 
 template<typename AxisT>
-inline uint32_t SpatialAllocator<AxisT>::level(NodeIndex index) const
+uint32_t SpatialAllocator<AxisT>::level(NodeIndex index) const
 {
 	return static_cast<uint32_t>(std::floor(static_cast<uint32_t>(std::log2(1 + 3 * index)) >> 1));
 }
 
+
+
 template<typename AxisT>
-inline AxisT SpatialAllocator<AxisT>::size(NodeIndex index) const
-{
-	return _size >> level(index);
+SpatialAllocator<AxisT>::NodeIndex SpatialAllocator<AxisT>::index_of_descendant(NodeIndex index, uint32_t skip_levels) {
+	assert(skip_levels > 0);
+	const auto current_level = level_from_index(index);
+	const auto level_offset = index - level_start_index(current_level);
+	const auto target_level_start = level_start_index(current_level + skip_levels);
+	const auto descendants_per_node = 1u << (2 * skip_levels);
+	const auto result = target_level_start + level_offset * descendants_per_node;
+	return result;
 }
 
 
