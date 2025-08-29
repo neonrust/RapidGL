2a47162631da05e91fc4c5a36aba8b7132c2945b
André Jonsson <andre.jonsson@dataductus.se>
2025-08-29 02:15:19 +0200

    Use RingBuffer for storage
    Removed the last(n), not possible to/easily implemented when using a
    ring buffer... :|

--- src/core/sample_window.h
+++ src/core/sample_window.h
@@ -1,11 +1,12 @@
 #pragma once
 
-#include "container_types.h"
+#include <algorithm>
+#include "ringbuffer.h"
 #include <cstddef>
 #include <numeric>
 
 
-template<typename T, size_t size>
+template<typename T, size_t size> requires (size > 0)
 class SampleWindow
 {
 public:
@@ -18,33 +19,28 @@ public:
 	[[nodiscard]] inline T min(T def=T{0}) const noexcept;
 	[[nodiscard]] inline T max(T def=T{0}) const noexcept;
 	[[nodiscard]] inline T first(T def=T{0}) const noexcept;
-	[[nodiscard]] inline T last(T def=T{0}) const noexcept;
+	// [[nodiscard]] inline std::basic_string_view<T> last(size_t count=T{0}) const noexcept;
 
 	[[nodiscard]] inline bool empty() const noexcept { return _samples.empty(); }
 	[[nodiscard]] inline size_t num_samples() const noexcept { return _samples.size(); }
 
 private:
-	small_vec<T, size> _samples;
+	RingBuffer<T, size> _samples;
 };
 
-template<typename T, size_t size>
+template<typename T, size_t size> requires (size > 0)
 inline void SampleWindow<T, size>::add(T value)
 {
-	if(_samples.size() == size)
-		_samples.erase(_samples.begin());
-	else if(_samples.capacity() == 0)
-		_samples.reserve(size);
-
-	_samples.push_back(value);
+	_samples.push(value);
 }
 
-template<typename T, size_t size>
+template<typename T, size_t size> requires (size > 0)
 inline void SampleWindow<T, size>::clear()
 {
 	_samples.clear();
 }
 
-template<typename T, size_t size>
+template<typename T, size_t size> requires (size > 0)
 inline T SampleWindow<T, size>::average(T def) const noexcept
 {
 	if(empty())
@@ -52,7 +48,7 @@ inline T SampleWindow<T, size>::average(T def) const noexcept
 	return sum() / num_samples();
 }
 
-template<typename T, size_t size>
+template<typename T, size_t size> requires (size > 0)
 inline T SampleWindow<T, size>::sum(T def) const noexcept
 {
 	if(empty())
@@ -60,7 +56,7 @@ inline T SampleWindow<T, size>::sum(T def) const noexcept
 	return std::accumulate(_samples.begin(), _samples.end(), T{ 0 });
 }
 
-template<typename T, size_t size>
+template<typename T, size_t size> requires (size > 0)
 inline T SampleWindow<T, size>::min(T def) const noexcept
 {
 	if(empty())
@@ -68,7 +64,7 @@ inline T SampleWindow<T, size>::min(T def) const noexcept
 	return std::min_element(_samples.begin(), _samples.end());
 }
 
-template<typename T, size_t size>
+template<typename T, size_t size> requires (size > 0)
 inline T SampleWindow<T, size>::max(T def) const noexcept
 {
 	if(empty())
@@ -76,7 +72,7 @@ inline T SampleWindow<T, size>::max(T def) const noexcept
 	return std::max_element(_samples.begin(), _samples.end());
 }
 
-template<typename T, size_t size>
+template<typename T, size_t size> requires (size > 0)
 inline T SampleWindow<T, size>::first(T def) const noexcept
 {
 	if(empty())
@@ -84,10 +80,15 @@ inline T SampleWindow<T, size>::first(T def) const noexcept
 	return _samples.front();
 }
 
-template<typename T, size_t size>
-inline T SampleWindow<T, size>::last(T def) const noexcept
-{
-	if(empty())
-		return def;
-	return _samples.back();;
-}
+// template<typename T, size_t size> requires (size > 0)
+// inline std::basic_string_view<T> SampleWindow<T, size>::last(size_t count) const noexcept
+// {
+// 	if(empty())
+// 		return {};
+// 	if(count == 0)
+// 		count = _samples.size();
+// 	else
+// 		count = std::min(count, _samples.size());
+
+// 	return std::basic_string_view<T>(&*(_samples.end() - count), count);
+// }
