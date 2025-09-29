0e4a2340e777aa2f3118c8dab89e2d046485b4d0
André Jonsson <andre.jonsson@dataductus.se>
2025-09-29 11:51:49 +0200

    The blending with previous froxel grid is resisting :|
    Moved some functions to a shared file: volumetrics.glh; more still to
    be moved.
    Provide previous view instead of view-projection.
    Removed thickness branch in the loop, there's already a check, don't
    need two.
    Even quite small contribution should be used; min_contribution =
    1e-5. Might just remove it entirely (1e-5 is on the edge of float
    precision anyway).

--- src/core/pp_volumetrics.cpp
+++ src/core/pp_volumetrics.cpp
@@ -78,11 +78,11 @@ void Volumetrics::inject(const Camera &camera) // TODO: View
 	const auto inv_view_projection = glm::inverse(view_projection);
 	_inject_shader.setUniform("u_inv_view_projection"sv, inv_view_projection);
 
-	static glm::mat4 prev_view_projection = glm::mat4(1);
+	static glm::mat4 prev_view = camera.viewTransform();  // use current view the first frame
 	// use current for the first frame (there's no "previous" yet)
-	_inject_shader.setUniform("u_prev_view_projection"sv, _frame == 1? view_projection: prev_view_projection);
+	_inject_shader.setUniform("u_prev_view"sv, prev_view);
 	// now there is :)
-	prev_view_projection = view_projection;
+	prev_view = camera.viewTransform();
 
 	// TODO: injection is quite slow, probably b/c it naively loops through all lights for all froxel "columns"
 	//   it's probably worth it to do a light culling pre-pass
