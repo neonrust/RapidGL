5785f253f60e2a74ffd80d5c1bfc6c3d84000d6d
André Jonsson <andre.jonsson@dataductus.se>
2026-01-24 13:03:21 +0100

    Bias slope scale might also be negative

--- src/demos/27_clustered_shading/clustered_shading_ui.cpp
+++ src/demos/27_clustered_shading/clustered_shading_ui.cpp
@@ -300,10 +300,10 @@ COL(1); ImGui::Text("%4ld µs", (time).count())
 
 		if(ImGui::CollapsingHeader("Shadows"))//, ImGuiTreeNodeFlags_DefaultOpen))
 		{
-			ImGui::SliderFloat("Bias constant",      &m_shadow_bias_constant,       -0.03f,  0.02f, "%.4f");
-			ImGui::SliderFloat("Bias slope scale",   &m_shadow_bias_slope_scale,     0.f,    5.f,   "%.2f");
 			ImGui::Checkbox("Colorize shadow slots", &_debug_colorize_shadows);
 			ImGui::SliderFloat("Shadow occlusion",   &m_shadow_occlusion,            0.f,    1.f,   "%.2f");
+			ImGui::SliderFloat("Bias constant",      &m_shadow_bias_constant,       -0.03f,  0.03f, "%.4f");
+			ImGui::SliderFloat("Bias slope scale",   &m_shadow_bias_slope_scale,    -5.f,    5.f,   "%.2f");
 			ImGui::SliderFloat("Bias slope power",   &m_shadow_bias_slope_power,     0.01f,  5.f,   "%.3f");
 			ImGui::SliderFloat("Bias dist. scale",   &m_shadow_bias_distance_scale, -0.01f,  0.01f, "%.4f");
 			ImGui::SliderFloat("Bias texel sz mix",  &m_shadow_bias_texel_size_mix,  0.f,    1.f,   "%.2f");
