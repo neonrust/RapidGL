9634b8654bcce8f514cfe5aaa435ea0970267b84
André Jonsson <andre.jonsson@dataductus.se>
2026-03-25 00:09:18 +0100

    Need vec3

--- src/core/constants.h
+++ src/core/constants.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glm/vec3.hpp>
+
 inline constexpr glm::vec3 AXIS_X { 1, 0, 0 };
 inline constexpr glm::vec3 AXIS_Y { 0, 1, 0 };
 inline constexpr glm::vec3 AXIS_Z { 0, 0, 1 };
