87ef0b2f62ccb686ffeb1867bbcd0f819a87c507
André Jonsson <andre.jonsson@dataductus.se>
2026-03-25 00:08:15 +0100

    Comment re using xx hash instead

--- src/core/hash_combine.h
+++ src/core/hash_combine.h
@@ -5,5 +5,6 @@
 template<typename T>
 [[nodiscard]] inline size_t hash_combine(std::size_t seed, const T& v)
 {
+	// TODO: change std::hash to someething like xxh3 ?
 	return seed ^ (std::hash<T>{}(v) + size_t(0x9e3779b97f4a7c15) + (seed << 6) + (seed >> 2));
 }
